0e06a60748c6093e9e39e21e90989e28d43182a2
Frank Rojas <rojasjhann7@gmail.com>
2025-06-13 06:06:32 -0500

    Agregar archivos de proyecto.

---
+++ clases_Metodos/Class1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace clases_Metodos
+{
+    public class Class1
+    {
+        public void animacion_de_entrada()
+        {
+
+            Console.WriteLine("CARGANDO...");
+            Thread.Sleep(1000);
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" A H  O  R  C  A  D  O ");
+            for (int i = 0; i < 8; i++)
+            {
+                if (i < 5)
+                {
+                    // Parte de suspenso: tonos graves descendentes
+                    int frecuencia = 600 - (i * 60); // 600, 540, 480, 420, 360
+                    Console.Beep(frecuencia, 600);
+                    Thread.Sleep(200);
+                }
+                else
+                {
+                    // Parte de susto: tonos agudos cortos
+                    Console.Beep(1000, 100);
+                    Thread.Sleep(100);
+                }
+            }
+            Thread.Sleep(100);
+            Thread.Sleep(500);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Preparando la soga...");
+            Thread.Sleep(1000);
+            Console.WriteLine("Buscando palabras secretas...");
+            Thread.Sleep(1000);
+            Console.WriteLine("¡Todo listo!");
+            Thread.Sleep(800);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Clear();
+            Console.WriteLine(" B I E N V E N I D O ");
+            Console.ForegroundColor = ConsoleColor.White;
+
+
+
+        }
+        public void animacion_de_salida()
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(" A D I O S ");
+            for (int i = 0; i < 8; i++)
+            {
+                if (i < 5)
+                {
+                    // Parte de suspenso: tonos graves descendentes
+                    int frecuencia = 600 - (i * 60); // 600, 540, 480, 420, 360
+                    Console.Beep(frecuencia, 600);
+                    Thread.Sleep(200);
+                }
+                else
+                {
+                    // Parte de susto: tonos agudos cortos
+                    Console.Beep(1000, 100);
+                    Thread.Sleep(100);
+                }
+            }
+            Thread.Sleep(100);
+            Thread.Sleep(500);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Gracias por jugar.");
+            Thread.Sleep(1000);
+            Console.WriteLine("Esperamos que hayas disfrutado del juego.");
+            Thread.Sleep(1000);
+            Console.WriteLine("¡Hasta la próxima!");
+            Thread.Sleep(800);
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+        public void proceso()
+        {
+
+        }
+    }
+}
---
+++ ejecutable/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using clases_Metodos;
+
+namespace ejecutable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           Class1 x = new Class1();
+            x.animacion_de_entrada();
+            Console.WriteLine("\nPresiona una tecla para comenzar...");
+            Console.ReadKey();
+            bool salir = false;
+
+            while (!salir)
+            {
+                Console.Clear();
+                Console.WriteLine(" MENÚ PRINCIPAL ");
+                Console.WriteLine("1. Iniciar Juego");
+                Console.WriteLine("2. Descripción del Juego");
+                Console.WriteLine("3. Créditos");
+                Console.WriteLine("4. Salir");
+                Console.Write("Selecciona una opción (1-4): ");
+
+                string opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        IniciarJuego();
+                        break;
+                    case "2":
+                        MostrarDescripcion();
+                        break;
+                    case "3":
+                        MostrarCreditos();
+                        break;
+                    case "4":
+                        salir = true;
+                        Console.WriteLine("Saliendo del juego... ¡Hasta pronto!");
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida. Presiona una tecla para continuar...");
+                        break;
+                }
+
+                if (!salir)
+                {
+                    Console.WriteLine("\nPresiona una tecla para volver al menú...");
+                    Console.ReadKey();
+                }
+            }
+        }
+
+        static void IniciarJuego()
+        {
+            Console.WriteLine("\n[Iniciando el juego...]");
+            Class1 x = new Class1();
+            x.proceso();
+        }
+
+        static void MostrarDescripcion()
+        {
+            Console.WriteLine("\n[Descripción del Juego]");
+            Console.WriteLine("Este es un emocionante juego de aventuras donde explorarás mundos desconocidos...");
+        }
+
+        static void MostrarCreditos()
+        {
+            Console.WriteLine("\n[Créditos]");
+            Console.WriteLine("Desarrollado por: Tu Nombre");
+            Console.WriteLine("Música y efectos: Nombre del compositor");
+            Console.WriteLine("Gracias por jugar :)");
+        }
+    
+    }
+}
